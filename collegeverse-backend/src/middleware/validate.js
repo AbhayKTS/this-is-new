8ece522254bde79d66c3b2b88c065fea51a88437
Chaos_Immortal <abhay88998@gmail.com>
2026-02-04 11:59:23 +0530

    this is new

--- collegeverse-backend/src/middleware/validate.js
+++ collegeverse-backend/src/middleware/validate.js
@@ -23,3 +23,34 @@ export const validateParams = (schema) => (req, res, next) => {
     next(err);
   }
 };
+
+export const validateQuery = (schema) => (req, res, next) => {
+  try {
+    req.validatedQuery = schema.parse(req.query);
+    next();
+  } catch (err) {
+    if (err instanceof ZodError) {
+      return res.status(400).json({ success: false, message: "Validation failed", errors: err.errors });
+    }
+    next(err);
+  }
+};
+
+// Combined validate helper for flexibility
+export const validate = ({ body, params, query }) => {
+  const middlewares = [];
+  if (body) middlewares.push(validateBody(body));
+  if (params) middlewares.push(validateParams(params));
+  if (query) middlewares.push(validateQuery(query));
+  
+  return (req, res, next) => {
+    const runMiddleware = (index) => {
+      if (index >= middlewares.length) return next();
+      middlewares[index](req, res, (err) => {
+        if (err) return next(err);
+        runMiddleware(index + 1);
+      });
+    };
+    runMiddleware(0);
+  };
+};
